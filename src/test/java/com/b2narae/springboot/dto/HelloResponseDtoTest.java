0fdea8cfea6396d731f6372523aef9da039dfdf9
Taekhwan <b2narae@gmail.com>
2021-09-19 13:04:26 +0900

    feat : Adding Dto and DtoTest

---
+++ src/test/java/com/b2narae/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.b2narae.springboot.dto;
+import com.b2narae.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_feature_test() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when - lombok 생성자 테스트
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then - getter setter 테스트
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
