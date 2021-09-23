f8aa7af3e106b8f8b769315e6e2f7040689db51c
Taekhwan <b2narae@gmail.com>
2021-09-23 10:41:32 +0900

    feat : Add updating posts

--- src/main/java/com/b2narae/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/b2narae/springboot/domain/posts/PostsRepository.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     // SpringDataJpa 에서 제공하지 않는 메소드는 쿼리로 작성 가능
-    @Query("SELECT p FROM posts p ORDER BY p.id DESC")
+    // 여기서 Posts를 posts로 적기만해도 에러 발생
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
 }
 
