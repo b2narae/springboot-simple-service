5524e77604a102d9a00fbfcafd03b988f0fd2dd4
Taekhwan <b2narae@gmail.com>
2021-09-23 10:02:07 +0900

    feat : Read all postsList in index UI

--- src/main/java/com/b2narae/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/b2narae/springboot/domain/posts/PostsRepository.java
@@ -1,5 +1,12 @@
 package com.b2narae.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PostsRepository extends JpaRepository<Posts, Long> {}
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+    // SpringDataJpa 에서 제공하지 않는 메소드는 쿼리로 작성 가능
+    @Query("SELECT p FROM posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+}
 
--- src/main/java/com/b2narae/springboot/service/posts/PostsService.java
+++ src/main/java/com/b2narae/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.b2narae.springboot.service.posts;
 
 import com.b2narae.springboot.domain.posts.Posts;
 import com.b2narae.springboot.domain.posts.PostsRepository;
+import com.b2narae.springboot.web.dto.PostsListResponseDto;
 import com.b2narae.springboot.web.dto.PostsResponseDto;
 import com.b2narae.springboot.web.dto.PostsSaveRequestDto;
 import com.b2narae.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -44,5 +48,13 @@ public class PostsService {
                 .orElseThrow(() -> new
                         IllegalArgumentException("There is no post of 'id : " + id));
         return new PostsResponseDto(entity);
+    }
+
+    // Transaction 범위는 유지하되 조회 기능만 남겨두어, 조회 속도가 개선됨
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
