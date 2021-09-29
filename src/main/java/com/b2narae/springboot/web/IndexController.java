0bbbda77e76ff997c430bbc8cd175dbf09ae8c6e
Taekhwan <b2narae@gmail.com>
2021-09-29 23:47:38 +0900

    refactor : remove redundant httpSession in IndexController

--- src/main/java/com/b2narae/springboot/web/IndexController.java
+++ src/main/java/com/b2narae/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.b2narae.springboot.web;
 
+import com.b2narae.springboot.config.auth.LoginUser;
 import com.b2narae.springboot.config.auth.dto.SessionUser;
 import com.b2narae.springboot.service.posts.PostsService;
 import com.b2narae.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,12 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+    // private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
