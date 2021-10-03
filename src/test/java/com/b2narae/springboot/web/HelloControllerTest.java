14703dff8014a4954b003eee8ab2ba7ad815b83f
Taekhwan <b2narae@gmail.com>
2021-10-03 10:49:37 +0900

    Test : seperate JpaAuditing from SpringBootApplication

--- src/test/java/com/b2narae/springboot/web/HelloControllerTest.java
+++ src/test/java/com/b2narae/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.b2narae.springboot.web;
+import com.b2narae.springboot.config.auth.SecurityConfig;
 import com.b2narae.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,11 +16,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class) // JPA 미지원
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }) // JPA 미지원
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_must_be_returned() throws Exception {
         String hello = "hello";
@@ -26,6 +34,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto_must_be_returned() throws Exception {
         String name = "hello";
