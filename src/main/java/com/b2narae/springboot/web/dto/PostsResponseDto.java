5aef7d6c5729472188e0267a25a1d43c5049d571
TAEKHWAN <b2narae@gmail.com>
2021-10-31 23:54:32 +0900

    Chores : Add PostsResponseDto Comment

--- src/main/java/com/b2narae/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/b2narae/springboot/web/dto/PostsResponseDto.java
@@ -4,6 +4,7 @@ import com.b2narae.springboot.domain.posts.Posts;
 import lombok.Getter;
 
 // Entity를 직접 사용하지 않고 Dto를 둔다.
+// 이유 : Entity는 테이블과 일치해야함, 이후 잦은 Join에서 분리시키기 위함
 @Getter
 public class PostsResponseDto {
 
